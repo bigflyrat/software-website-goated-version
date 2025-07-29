02c4b756d78c3e1a3dde06151187c29a42d3a464
Artin Raoofi <raoofiartin@gmail.com>
2025-07-30 09:35:33 +1000

    more stats

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let highest = 0
 let total = 0
+let num = 0
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -11,9 +12,12 @@ const buttonPressed = function() {
         for (let i = 0; i < 100; i++) {
             var rng = Math.random()
             var roll = 1/rng
+            num += 1
             document.getElementById("robuckContainer").innerHTML = "<p>you gained <b>" + roll + "</b> robuck!!!</p>"
             total += roll
             document.getElementById("totalRobuck").innerHTML = "Total: <b>" + total + "</b>"
+            document.getElementById("totalRolls").innerHTML = "Rolls: <b>" + num + "</b>"
+            document.getElementById("averageRobuck").innerHTML = "Average: <b>" + total/num + "</b>"
             if (roll >= highest) {
                 highest = roll
                 document.getElementById("highestRoll").innerHTML = "Highest roll: <b>" + highest + "</b> (RNG: " + 1/highest + ")"
