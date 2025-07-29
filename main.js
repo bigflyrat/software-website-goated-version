1dfb3a26e5a81ba2dcd627410de2717c9198dbaf
Artin Raoofi <raoofiartin@gmail.com>
2025-07-30 08:44:12 +1000

    rng

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+let highest = 0
+let total = 0
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const buttonPressed = function() {
-    document.body.innerHTML += "<p>you gained 1 robuck!!!</p>"
+    // buttonPressed = function(){}
+    setInterval(() => {
+        var rng = Math.random()
+        var roll = 1/rng
+        document.getElementById("robuckContainer").innerHTML = "<p>you gained <b>" + roll + "</b> robuck!!!</p>"
+        total += roll
+        document.getElementById("totalRobuck").innerHTML = "Total: <b>" + total + "</b>"
+        if (roll >= highest) {
+            highest = roll
+            document.getElementById("highestRoll").innerHTML = "Highest roll: <b>" + highest + "</b> (RNG: " + 1/highest + ")"
+        }
+    }, 1);
 }
