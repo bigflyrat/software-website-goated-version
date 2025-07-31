c44586104bbafc02f14eda4e6974f9e47e90d432
Artin Raoofi <raoofiartin@gmail.com>
2025-08-01 08:35:09 +1000

    good

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ const buttonPressed = function() {
     }
     started = true
     document.getElementById("freeRobuck").innerHTML = "";
+    document.getElementById("subwaySurfers").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/zZ7AimPACzc?autoplay=1&mute=0&showinfo=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen id="subwayVideo"></iframe><br/>
+            <button onclick="killVideo()">remove subway surfers</button>`
     setInterval(() => {
         let lastRoll = 0;
         let batchHighest = highest;
