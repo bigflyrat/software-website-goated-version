91cb3b370ae1a10177c6432515ca892cd5bef5bf
Artin Raoofi <raoofiartin@gmail.com>
2025-08-01 11:33:28 +1000

    added logs

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let highest = 0
-let lowest = 100000000
+let lowest = Infinity
 let total = 0
 let num = 0
 let luck = 1
 let time = 0
 let started = false
-let subwaySurfers = `<iframe width="560" height="315" src="https://www.youtube.com/embed/zZ7AimPACzc?autoplay=1&mute=0&showinfo=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen id="subwayVideo"></iframe><br/>`
+let subwaySurfers = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/zZ7AimPACzc?autoplay=1&mute=0&showinfo=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen id="subwayVideo"></iframe><br/>`
 
-const formatSecondsToTime = function(seconds) {
+const formatSecondsToTime = function (seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
@@ -22,14 +22,23 @@ const formatSecondsToTime = function(seconds) {
     return formattedTime;
 }
 
-const buttonPressed = function() {
+const pushToLogs = function (text) {
+    let p = document.createElement('p')
+    p.innerHTML = `<b>${formatSecondsToTime(time)}</b>: ${text}`
+    p.classList.add("animate__animated", "animate__fadeInDown")
+    document.getElementById("logs").prepend(p);
+}
+
+const buttonPressed = function () {
     if (started) {
         return
     }
     started = true
     document.getElementById("freeRobuck").innerHTML = "";
     document.getElementById("subwaySurfers").innerHTML = subwaySurfers
+    document.getElementById("logText").innerHTML = "<b>Logs:</b>"
     document.getElementById("subwayButton").innerHTML = `<button onclick="killVideo()">toggle subway surfers</button>`
+    document.getElementById("logsContainerContainer").innerHTML = `<div id="logs"></div>`
     setInterval(() => {
         let lastRoll = 0;
         let batchHighest = highest;
@@ -38,15 +47,17 @@ const buttonPressed = function() {
         let batchTotalSum = total;
         for (let i = 0; i < 500; i++) {
             var rng = Math.pow(Math.random(), luck)
-            var roll = 1/rng
+            var roll = 1 / rng
             batchNum += 1
             batchTotalSum += roll
             lastRoll = roll
             if (roll > batchHighest) {
                 batchHighest = roll;
+                pushToLogs(`New Highest: <b>${batchHighest}</b>`)
             }
             if (roll < batchLowest) {
-                batchLowest = roll;
+                batchLowest = roll
+                pushToLogs(`New Lowest: <b>${batchLowest}</b>`)
             }
         }
         num = batchNum;
@@ -60,7 +71,7 @@ const buttonPressed = function() {
         document.getElementById("highestRoll").innerHTML = `Highest roll: <b>${highest}</b> (RNG: ${1/highest})`;
         document.getElementById("lowestRoll").innerHTML = `Lowest roll: <b>${lowest}</b> (RNG: ${1/lowest})`;
         let overOrUnder = "Under"
-        if (highest/num >= 1) {
+        if (highest / num >= 1) {
             overOrUnder = "Over"
         }
         document.getElementById("onOdds").innerHTML = `% ${overOrUnder} Odds: <b>${((highest/num) * 100).toFixed(0)}%</b>`;
@@ -74,7 +85,7 @@ const buttonPressed = function() {
     }, 1000);
 }
 
-const killVideo = function() {
+const killVideo = function () {
     if (document.getElementById("subwaySurfers").innerHTML == "") {
         document.getElementById("subwaySurfers").innerHTML = subwaySurfers
     } else {
